973e22e5d37db6bf955cc0bc66beb2472e8fa281
Dean Kago <martokhago@gmail.com>
2022-06-05 18:10:25 +0300

    created super user

--- insta/models.py
+++ insta/models.py
@@ -8,3 +8,6 @@ class Post(models.Model):
   author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
   description = models.TextField()
   date_created = models.DateTimeField(default=timezone.now)
+
+  def __str__(self):
+    return self.description
